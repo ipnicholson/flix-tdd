06e030bda5c00a97d07923612544a6eaece3a46b
Ian Nicholson <ipnicholson@gmail.com>
2018-01-14 12:46:57 -0800

    formatting

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -5,6 +5,7 @@ describe "Viewing the list of movies" do
     visit movies_url
 
     expect(page).to have_text("3 Movies") # page is an object from Capybara
+    
     expect(page).to have_text("Movie 1")
     expect(page).to have_text("Movie 2")
     expect(page).to have_text("Movie 3")
