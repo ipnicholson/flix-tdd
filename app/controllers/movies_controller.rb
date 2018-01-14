d0cf423050fddd4f1c2c5befd4705cafa215edf2
Ian Nicholson <ipnicholson@gmail.com>
2018-01-14 12:33:57 -0800

    progress commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,9 @@
 class MoviesController < ApplicationController
   def index
+    @movies = [
+      'Movie 1',
+      'Movie 2',
+      'Movie 3'
+    ]
   end
 end
--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 describe "Viewing the list of movies" do
-  it "shows the event" do
+  it "shows the events" do
     visit "http://example.com/movies"
 
-    expect(page).to have_text("3 Events") # page is an object from Capybara
+    expect(page).to have_text("3 Movies") # page is an object from Capybara
+    expect(page).to have_text("Movie 1")
+    expect(page).to have_text("Movie 2")
+    expect(page).to have_text("Movie 3")
   end
 end
