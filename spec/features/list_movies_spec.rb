3c4e1a9a91e73acb7c51cb1d3288a4d1c76f6ba1
Ian Nicholson <ipnicholson@gmail.com>
2018-01-15 13:23:00 -0800

    test for display of rating, description, and released_on from movie1 object

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -26,5 +26,9 @@ describe "Viewing the list of movies" do
     expect(page).to have_text(movie1.title)
     expect(page).to have_text(movie2.title)
     expect(page).to have_text(movie3.title)
+
+    expect(page).to have_text(movie1.rating)
+    expect(page).to have_text(movie1.description[0...9])
+    expect(page).to have_text(movie1.released_on)
   end
 end
