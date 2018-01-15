7f7d40c1a7e3b169ed1ae7baca46db65169e4554
Ian Nicholson <ipnicholson@gmail.com>
2018-01-15 13:16:20 -0800

    green

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -8,13 +8,13 @@ describe "Viewing the list of movies" do
                           description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
                           released_on: "2014-11-07")
 
-    movie1 = Movie.create(title: "Dunkirk",
+    movie2 = Movie.create(title: "Dunkirk",
                           rating: "PG-13",
                           total_gross: "525573161",
                           description: "Allied soldiers from Belgium, the British Empire and France are surrounded by the German Army, and evacuated during a fierce battle in World War II.",
                           released_on: "2017-07-21")
 
-    movie1 = Movie.create(title: "Kubo and the Two Strings",
+    movie3 = Movie.create(title: "Kubo and the Two Strings",
                           rating: "PG",
                           total_gross: "76257813",
                           description: "A young boy named Kubo must locate a magical suit of armour worn by his late father in order to defeat a vengeful spirit from the past.",
