2bb1b2bf6835ebe6b93db6eea14a88549d3b9d10
Ian Nicholson <ipnicholson@gmail.com>
2018-01-15 19:56:00 -0800

    refactor

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -8,7 +8,7 @@ describe "Showing an individual movie" do
                           description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
                           released_on: "2014-11-07")
 
-    visit "http://example.com/movies/#{movie.id}"
+    visit movie_url(movie)
 
     expect(page).to have_text(movie.title)
     expect(page).to have_text(movie.rating)
