bee285b23648b6df92d51da9ca1c8b4f9ed7cac4
Ian Nicholson <ipnicholson@gmail.com>
2018-01-17 12:47:02 -0800

    initial commit, view helper

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+  def format_total_gross(movie)
+    if movie.total_gross_zero?
+      "Unknown"
+    else
+      number_to_currency(movie.total_gross, precision: 0)
+    end
+  end
 end
