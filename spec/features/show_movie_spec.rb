6ec36a7734aa5951c9a088ee189dceea4db957f9
Ian Nicholson <ipnicholson@gmail.com>
2018-01-17 12:15:48 -0800

    refactor

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -2,17 +2,13 @@ require 'rails_helper'
 
 describe "Viewing an individual movie" do
   it "shows the movie's details" do
-    movie = Movie.create(title: "Interstellar",
-                          rating: "PG-13",
-                          total_gross: 675020017,
-                          description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-                          released_on: "2014-11-07")
+    movie = Movie.create(movie_attributes(total_gross: 123456))
 
     visit movie_url(movie)
 
     expect(page).to have_text(movie.title)
     expect(page).to have_text(movie.rating)
-    expect(page).to have_text("$675,020,017")
+    expect(page).to have_text("$123,456")
     expect(page).to have_text(movie.description)
     expect(page).to have_text(movie.released_on)
   end
