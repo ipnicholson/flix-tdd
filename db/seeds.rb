30c0b178ee7183cc39e862af1efc49e65ca29f9e
Ian Nicholson <ipnicholson@gmail.com>
2018-01-17 13:15:27 -0800

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create!([
+  {
+    title: "Interstellar",
+    rating: "PG-13",
+    total_gross: "675020017",
+    description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    released_on: "2014-11-07"
+  },
+  {
+    title: "Dunkirk",
+    rating: "PG-13",
+    total_gross: "525573161",
+    description: "Allied soldiers from Belgium, the British Empire and France are surrounded by the German Army, and evacuated during a fierce battle in World War II.",
+    released_on: "2017-07-21"
+  },
+  {
+    title: "Kubo and the Two Strings",
+    rating: "PG",
+    total_gross: "76257813",
+    description: "A young boy named Kubo must locate a magical suit of armour worn by his late father in order to defeat a vengeful spirit from the past.",
+    released_on: "2016-08-19"
+  }
+])
+
+
+
