e9fc97da2b0b4db6a8a775bfce0651f75da1bc10
Ian Nicholson <ipnicholson@gmail.com>
2018-01-17 12:22:31 -0800

    refactor

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 describe "Viewing an individual movie" do
   it "shows the movie's details" do
-    movie = Movie.create(movie_attributes(total_gross: 123456))
+    movie = Movie.create(movie_attributes)
 
     visit movie_url(movie)
 
     expect(page).to have_text(movie.title)
     expect(page).to have_text(movie.rating)
-    expect(page).to have_text("$123,456")
     expect(page).to have_text(movie.description)
     expect(page).to have_text(movie.released_on)
   end
+
+  it "shows the total gross if not $0" do
+    movie = Movie.create(movie_attributes(total_gross: 123456))
+
+    visit movie_url(movie)
+
+    expect(page).to have_text("$123,456")
+  end
+  it "shows 'Unknown' if total gross is $0"
 end
