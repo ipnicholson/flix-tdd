5cd1aa4fa153462c2d12ca8e2317ba183d28a388
Ian Nicholson <ipnicholson@gmail.com>
2018-01-17 10:49:51 -0800

    green, add navigation

---
+++ spec/features/navigate_movie_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "Navigating movies " do
+  it "allows nivagation from detail page to listing page" do
+    movie = Movie.create(title: "Interstellar",
+                          rating: "PG-13",
+                          total_gross: 675020017,
+                          description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+                          released_on: "2014-11-07")
+
+    visit movie_url(movie)
+
+    click_link "All Movies"
+
+    expect(current_path).to eq(movies_path)
+  end
+  
+  it "allows navigation from listing page to detail page" do
+    
+  end
+
+end
