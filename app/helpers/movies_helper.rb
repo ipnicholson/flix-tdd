feb3629795038e8872ffa56aa2e2a198bbc691a4
Ian Nicholson <ipnicholson@gmail.com>
2018-01-17 13:06:05 -0800

    bugfix

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,7 +1,7 @@
 module MoviesHelper
   def format_total_gross(movie)
     if movie.total_gross_zero?
-      content_tag(:em "Unknown")
+      content_tag(:em, "Unknown")
     else
       number_to_currency(movie.total_gross, precision: 0)
     end
