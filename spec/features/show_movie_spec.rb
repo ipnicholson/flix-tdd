3d3e332a5ce56cbeb2e3a5bc2d822da07de52032
Ian Nicholson <ipnicholson@gmail.com>
2018-01-17 10:47:00 -0800

    refactor, change total_gross to number instead of string

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -4,7 +4,7 @@ describe "Viewing an individual movie" do
   it "shows the movie's details" do
     movie = Movie.create(title: "Interstellar",
                           rating: "PG-13",
-                          total_gross: "675020017",
+                          total_gross: 675020017,
                           description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
                           released_on: "2014-11-07")
 
