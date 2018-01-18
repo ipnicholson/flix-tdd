1c0213c408a33f6c00f98a1d9db51b5ea7343884
Ian Nicholson <ipnicholson@gmail.com>
2018-01-17 19:56:44 -0800

    progress commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,8 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    fail
+  end
 end
--- spec/features/edit_movie_spec.rb
+++ spec/features/edit_movie_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Editing a movie" do
-  it "shows the existing details" do
+  it "shows the existing details, updates the movie, and shows the movie's updated details" do
     movie = Movie.create(movie_attributes)
 
     visit movie_url(movie)
@@ -11,5 +11,13 @@ describe "Editing a movie" do
     expect(current_path).to eq(edit_movie_path(movie))
 
     expect(find_field("Title").value).to eq(movie.title)
+
+    fill_in "Title", with: "Updated Movie Title"
+
+    click_button ("Update Movie")
+
+    expect(current_path).to eq(movie_path(movie))
+
+    expect(page).to have_text("Updated Movie Title")
   end
 end
