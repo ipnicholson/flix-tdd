bb23fad1a75dab2068621ef86192935dff6105f1
Ian Nicholson <ipnicholson@gmail.com>
2018-01-19 11:31:44 -0800

    add tests

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -1,6 +1,30 @@
 require 'rails_helper'
 
-describe "Creating a movie" do
-  it "asks for the movie's details"
+describe "Creating a new movie" do
+  it "has blank inputs for movie details" do
+    visit new_movie_url
+
+    expect(find_field("Title").value).to eq(nil)
+    expect(find_field("Description").value).to eq("")
+    expect(find_field("Rating").value).to eq(nil)
+    expect(find_field("Released on").value).to eq(nil)
+    expect(find_field("Total gross").value).to eq(nil)
+  end
+
+  it "saves the movie and shows the new movie's details" do
+    visit new_movie_url
+
+    expect(current_path).to eq(new_movie_path)
+
+    fill_in "Title",        with: "Test Movie Title"
+    fill_in "Description",  with: "Test movie description"
+    fill_in "Released on",  with: "2017-01-01"    
+    fill_in "Rating",       with: "Test movie rating"
+    fill_in "Total gross",  with: 123456789
+
+    click_button ("Create Movie")
+
+    expect(page).to have_text("Test Movie Title")
+  end
 
 end
