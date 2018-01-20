f7d54b8978a3b927ae0033c4854eea7ca30af37c
Ian Nicholson <ipnicholson@gmail.com>
2018-01-19 21:11:34 -0800

    total_gross should be number not string, add test movie from future

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -4,21 +4,27 @@ describe "Viewing the list of movies" do
   it "shows the movies" do
     movie1 = Movie.create(title: "Interstellar",
                           rating: "PG-13",
-                          total_gross: "675020017",
+                          total_gross: 675020017,
                           description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
                           released_on: "2014-11-07")
 
     movie2 = Movie.create(title: "Dunkirk",
                           rating: "PG-13",
-                          total_gross: "525573161",
+                          total_gross: 525573161,
                           description: "Allied soldiers from Belgium, the British Empire and France are surrounded by the German Army, and evacuated during a fierce battle in World War II.",
                           released_on: "2017-07-21")
 
     movie3 = Movie.create(title: "Kubo and the Two Strings",
                           rating: "PG",
-                          total_gross: "76257813",
+                          total_gross: 76257813,
                           description: "A young boy named Kubo must locate a magical suit of armour worn by his late father in order to defeat a vengeful spirit from the past.",
                           released_on: "2016-08-19")
+    
+    movie4 = Movie.create(title: "Sample Future Movie",
+                          rating: "PG",
+                          total_gross: 123456789,
+                          description: "Sample description",
+                          released_on: "2222-11-22")
 
     visit movies_url
 
