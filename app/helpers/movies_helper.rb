6e1346abc611d0eeea92807bfb30dcc4f4176de7
Ian Nicholson <ipnicholson@gmail.com>
2018-01-23 14:58:32 -0800

    add view helper method for movie poster

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -6,4 +6,12 @@ module MoviesHelper
       number_to_currency(movie.total_gross, precision: 0)
     end
   end
+
+  def display_movie_poster(movie)
+    if movie.image_file_name.blank?
+      <%= image_tag "placeholder.png" %>
+    else
+      <%= image_tag movie.image_file_name %>
+    end
+  end
 end
