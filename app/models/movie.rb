024a80a65602f4bee7da5a89c9cb2eddbdbf8b61
Ian Nicholson <ipnicholson@gmail.com>
2018-01-22 16:11:26 -0800

    add released and recently_added methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,13 @@ class Movie < ApplicationRecord
   def total_gross_zero?
     total_gross.blank? || total_gross.zero?
   end
+
+  def self.released
+    date = Time.now
+    Movie.where('released_on <= ?', date).order("released_on desc")
+  end
+
+  def self.recently_added
+    order("created_at desc")
+  end
 end
