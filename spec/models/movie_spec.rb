8859d07a2cba2ba50dfe8887b4dc34a06686db9c
Ian Nicholson <ipnicholson@gmail.com>
2018-01-22 16:12:52 -0800

    add tests for released and recently_added methods

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -18,4 +18,32 @@ describe "A movie" do
     
     expect(movie.total_gross_zero?).to eq(true)
   end
+
+  it "is released when the released date is in the past" do
+    movie = Movie.create(movie_attributes(released_on: 1.month.ago))
+
+    expect(Movie.released).to include(movie)
+  end
+
+  it "is not released when the release date is in the future" do
+    movie = Movie.create(movie_attributes(released_on: 1.month.from_now))
+
+    expect(Movie.released).to_not include(movie)
+  end
+
+  it "returns released movies ordered with the most recently-released movie first" do
+    movie1 = Movie.create(movie_attributes(released_on: 3.months.ago))
+    movie2 = Movie.create(movie_attributes(released_on: 2.months.ago))
+    movie3 = Movie.create(movie_attributes(released_on: 1.month.ago))
+
+    expect(Movie.released).to eq([movie3, movie2, movie1])
+  end
+
+  it "returns recently-added movies with the most recently-added movie first" do
+    movie1 = Movie.create(movie_attributes(title: "Added First"))
+    movie2 = Movie.create(movie_attributes(title: "Added Second"))
+    movie3 = Movie.create(movie_attributes(title: "Added Third"))
+
+    expect(Movie.recently_added).to eq([movie3, movie2, movie1])
+  end
 end
