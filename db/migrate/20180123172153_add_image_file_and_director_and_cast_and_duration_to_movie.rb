4432c6f735fced1e3afadd8cc9d745e25ce84a86
Ian Nicholson <ipnicholson@gmail.com>
2018-01-24 10:19:02 -0800

    GREEN, bugfix

--- db/migrate/20180123172153_add_image_file_and_director_and_cast_and_duration_to_movie.rb
+++ db/migrate/20180123172153_add_image_file_and_director_and_cast_and_duration_to_movie.rb
@@ -1,8 +1,8 @@
 class AddImageFileAndDirectorAndCastAndDurationToMovie < ActiveRecord::Migration[5.1]
   def change
-    add_column :movies, :image_file_name, :string
-    add_column :movies, :director, :string
-    add_column :movies, :cast, :string
-    add_column :movies, :duration, :string
+    # add_column :movies, :image_file_name, :string
+    # add_column :movies, :director, :string
+    # add_column :movies, :cast, :string
+    # add_column :movies, :duration, :string
   end
 end
