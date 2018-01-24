a1abf9c06b832d99d743a5baae19c8fe81a30b1c
Ian Nicholson <ipnicholson@gmail.com>
2018-01-24 09:21:33 -0800

    green, add fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115211254) do
+ActiveRecord::Schema.define(version: 20180124004848) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20180115211254) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.date "released_on"
+    t.string "director"
+    t.string "cast"
+    t.string "duration"
+    t.string "image_file_name", default: ""
   end
 
 end
