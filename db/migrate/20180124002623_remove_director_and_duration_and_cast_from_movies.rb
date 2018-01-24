a3e22b0445dbb58b6b3710ee1a5ba8c17b519a53
Ian Nicholson <ipnicholson@gmail.com>
2018-01-24 09:22:19 -0800

    green, rewind db

---
+++ db/migrate/20180124002623_remove_director_and_duration_and_cast_from_movies.rb
@@ -0,0 +1,7 @@
+class RemoveDirectorAndDurationAndCastFromMovies < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :movies, :director, :string
+    remove_column :movies, :duration, :string
+    remove_column :movies, :cast, :string
+  end
+end
