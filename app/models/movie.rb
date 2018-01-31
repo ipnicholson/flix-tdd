90dc83ff36e32495b2ae0d830a71bb3ae5cc7bf6
Ian Nicholson <ipnicholson@gmail.com>
2018-01-31 15:49:14 -0800

    add validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,16 @@
 class Movie < ApplicationRecord
+
+  validates :title, :rating, :released_on, presence: true
+
+  validates :description, length: { minimum: 25 }
+  
+  validates :total_gross, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validates :image_file_name, allow_blank: true, format: {
+    with: /\w+.(gif|jpg|png)/i,
+    message: "Must be .gif, .jpg, or .png file name"
+  }
+
   def total_gross_zero?
     total_gross.blank? || total_gross.zero?
   end
