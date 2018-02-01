b401537f35c901c5eb4ef1a2549f5e01baa796af
Ian Nicholson <ipnicholson@gmail.com>
2018-01-31 16:33:53 -0800

    Green, add tests for validations

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -50,10 +50,46 @@ describe "A movie" do
   it "accepts a positive total gross" do
     movie = Movie.create(movie_attributes(total_gross: 100000))
 
+    # movie.valid?
+
     expect(movie.errors[:total_gross].any?).to eq(false)
   end
 
-  it "has a valid image file name" do
-    
+  it "requires a description over 24 characters" do
+    movie = Movie.create(movie_attributes(description: "X" * 24))
+
+    # movie.valid?
+
+    expect(movie.errors[:description].any?).to eq(true)
+  end
+
+  it "accepts a 0 total gross" do
+    movie = Movie.create(movie_attributes(total_gross: 0))
+
+    expect(movie.errors[:total_gross].any?).to eq(false)
+  end
+  
+  it "accepts a negative total gross" do
+    movie = Movie.create(movie_attributes(total_gross: -100000))
+  
+    # movie.valid?
+  
+    expect(movie.errors[:total_gross].any?).to eq(true)
+  end
+
+  it "accepts properly formatted image file names" do
+    file_names = %w[e.png movie.png movie.jpg movie.gif MOVIE.GIF]
+    file_names.each do |file_name|
+      movie = Movie.create(movie_attributes(image_file_name: file_name))
+      expect(movie.errors[:image_file_name].any?).to eq(false)
+    end
+  end
+  
+  it "rejects improperly formatted image file names" do
+    file_names = %w[movie .png .jpg .gif movie.doc]
+    file_names.each do |file_name|
+      movie = Movie.create(movie_attributes(image_file_name: file_name))
+      expect(movie.errors[:image_file_name].any?).to eq(true)
+    end
   end
 end
