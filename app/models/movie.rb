e98bc7454be6d80965b40dcaf4f566b9752b62a5
Ian Nicholson <ipnicholson@gmail.com>
2018-02-01 10:41:31 -0800

    green, add validation for ratings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,9 @@ class Movie < ApplicationRecord
 
   validates :title, :rating, :released_on, presence: true
 
+  RATINGS = %w[G PG PG-13 R NC-17] << "Not Rated"
+  validates :rating, inclusion: { in: RATINGS }
+  
   validates :description, length: { minimum: 25 }
   
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
@@ -10,6 +13,7 @@ class Movie < ApplicationRecord
     with: /\w+.(gif|jpg|png)/i,
     message: "Must be .gif, .jpg, or .png file name"
   }
+
 
   def total_gross_zero?
     total_gross.blank? || total_gross.zero?
