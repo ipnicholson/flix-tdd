21b7ff587e8f6fd19bca4ba1f688b36c1171bdd5
Ian Nicholson <ipnicholson@gmail.com>
2018-03-06 18:08:44 -0800

    add new actions, permitted attributes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,35 @@ class ReviewsController < ApplicationController
     @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new(review_params)
+
+    if @review.save
+      redirect_to movie_reviews_path(@movie), notice: "Review sucessfully added!"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.find([:id])
+  end
+
+  private
+    def review_params
+      review_params = params.require(:review).
+        permit( :name,
+                :stars,
+                :comment
+        )
+    end
+
 end
