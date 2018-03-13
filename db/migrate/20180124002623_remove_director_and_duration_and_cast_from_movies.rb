b56ea4b6dfd695a8c57019ddf4c687d90effc1ad
Ian Nicholson <ipnicholson@gmail.com>
2018-03-13 14:37:23 -0700

    fix migration

--- db/migrate/20180124002623_remove_director_and_duration_and_cast_from_movies.rb
+++ db/migrate/20180124002623_remove_director_and_duration_and_cast_from_movies.rb
@@ -2,6 +2,6 @@ class RemoveDirectorAndDurationAndCastFromMovies < ActiveRecord::Migration[5.1]
   def change
     # remove_column :movies, :director, :string
     # remove_column :movies, :duration, :string
-    remove_column :movies, :cast, :string
+    # remove_column :movies, :cast, :string
   end
 end
