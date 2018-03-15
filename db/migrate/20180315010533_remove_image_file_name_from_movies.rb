bcec5a096d285e1f7111b40b580b2a22b2f22f33
Ian Nicholson <ipnicholson@gmail.com>
2018-03-14 18:07:53 -0700

    remove unused column in db

--- db/migrate/20180315010533_remove_image_file_name_from_movies.rb
+++ db/migrate/20180315010533_remove_image_file_name_from_movies.rb
@@ -1,5 +1,5 @@
 class RemoveImageFileNameFromMovies < ActiveRecord::Migration[5.1]
   def change
-    # remove_column :movies, :image_file_name, :string
+    remove_column :movies, :image_file_name, :string
   end
 end
