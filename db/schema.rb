de12606958ff2a224d3284fd8bf3894890051d9b
Ian Nicholson <ipnicholson@gmail.com>
2018-04-02 17:10:03 -0700

    create user resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315010533) do
+ActiveRecord::Schema.define(version: 20180402230903) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20180315010533) do
     t.integer "stars"
     t.text "comment"
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
