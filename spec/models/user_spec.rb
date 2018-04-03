7dcb77c93d88624b0d427fd74adc01ea4410e15a
Ian Nicholson <ipnicholson@gmail.com>
2018-04-03 10:07:14 -0700

    add tests for model and validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "A User" do
+  it "requires a name" do
+    u = User.new
+
+    u.name = ""
+
+    u.save
+
+    expect(u.errors[:name].any?).to be(true)
+  end
+
+  it "requires a valid name" do
+    u = User.new
+
+    u.name = "Emmylou"
+
+    u.save
+
+    expect(u.errors[:name].any?).to be(false)
+  end
+
+  it "requires an email" do
+    u = User.new
+
+    u.email = ""
+
+    u.save
+
+    expect(u.errors[:email].any?).to be(true)
+  end
+
+  it "accepts properly formatted email addresses" do
+    emails = %w[emmylou@firstaidkit.com my.user@example.co]
+
+    emails.each do |email|
+      u = User.new
+      u.email = email
+      u.save
+
+      expect(u.errors[:email].any?).to be(false)
+    end
+  end
+
+  it "rejects improperly formatted email addresses" do
+    emails = %w[emmy@ @firstaidkit.com @]
+
+    emails.each do |email|
+      u = User.new(email: email)
+      u.save
+
+      expect(u.errors[:email].any?).to be(true)
+    end
+  end
+
 end
